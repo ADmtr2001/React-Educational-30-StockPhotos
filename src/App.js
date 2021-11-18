f8285638ce8456c77732663d83e3c77577161f5d
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-18 21:41:48 +0200

    add photo list rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -12,11 +13,12 @@ function App() {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=eMXlgH8ShL6P4O7zgVRrlLWRamycCyCyQw8kMVsNzc4`
+    url = `${mainUrl}${clientID}`
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -27,7 +29,30 @@ function App() {
     fetchImages();
   }, []);
 
-  return <h2>stock photos starter</h2>
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default App
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input type='text' placeholder='search' className='form-input' />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  );
+};
+
+export default App;
