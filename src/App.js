4f3dd47d73bb440d6208ad6f0317e706d7ab82de
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-18 20:59:08 +0200

    add data fetching

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}?client_id=eMXlgH8ShL6P4O7zgVRrlLWRamycCyCyQw8kMVsNzc4`
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return <h2>stock photos starter</h2>
 }
 
